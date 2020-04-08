9992a119bc2ecd595094a8413e81a2f894b18bcc
Vanesa Reinerth <vanesa@getbowtied.com>
2020-04-08 09:58:38 +0300

    v.1.1.3

--- mrtailor-portfolio.php
+++ mrtailor-portfolio.php
@@ -4,13 +4,13 @@
  * Plugin Name:       		Mr. Tailor Portfolio Addon
  * Plugin URI:        		https://mrtailor.wp-theme.design/
  * Description:       		Extends the functionality of your WordPress site by adding a 'Portfolio' custom post type allowing you to organize and showcase you your work or products.
- * Version:           		1.1.2
+ * Version:           		1.1.3
  * Author:            		GetBowtied
  * Author URI:				https://getbowtied.com
  * Text Domain:				mr-tailor-portfolio
  * Domain Path:				/languages/
  * Requires at least: 		5.0
- * Tested up to: 			5.3.1
+ * Tested up to: 			5.4
  *
  * @package  Mr. Tailor Portfolio
  * @author   GetBowtied
